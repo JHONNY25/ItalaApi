385e05b00b48ca2f997c0d6b0bde6a8dd4fefed5
JHONNY25 <37971335+JHONNY25@users.noreply.github.com>
2020-09-02 10:38:34 -0600

    create model PaymentsMethodsUser

---
+++ app/PaymentsMethodsUser.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PaymentsMethodsUser extends Model
+{
+    protected $table = 'payments_methods_user';
+    protected $fillable = [
+        'payment_method_id',
+        'user_id',
+        'card_number',
+        'name_number',
+    ];
+
+    use SoftDeletes;
+}
