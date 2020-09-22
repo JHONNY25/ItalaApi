edf17790946a1ce2a5e8a81cbf0f2df134f5548b
Jonathan Rodríguez Hernández <37971335+JHONNY25@users.noreply.github.com>
2020-09-21 20:49:36 -0600

    change name

--- app/UserPaymentsMethods.php
+++ app/UserPaymentsMethods.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class PaymentsMethodsUser extends Model
+class UserPaymentsMethods extends Model
 {
     protected $table = 'payments_methods_user';
     protected $fillable = [
